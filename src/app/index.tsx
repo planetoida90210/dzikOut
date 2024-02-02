48f22b5d4059df69331353ae28969ed52429e86c
Michał Bryk <galeriakoloru90210@gmail.com>
2024-02-02 23:09:47 +0100

    fetch data from graphql

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,46 @@
-import { FlashList } from '@shopify/flash-list';
-import { StyleSheet, View } from 'react-native';
+import { ActivityIndicator, StyleSheet, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 
-import exercises from '@/assets/data/exercises.json';
+import { FlashList } from '@shopify/flash-list';
+import { gql, request } from 'graphql-request';
+import { useQuery } from '@tanstack/react-query';
+
 import { renderItem } from '@/components/ExerciseListItem';
 
-export default function App() {
+const exerciseQuery = gql`
+  query exercises($muscle: String, $name: String) {
+    exercises(muscle: $muscle, name: $name) {
+      name
+      muscle
+    }
+  }
+`;
+
+const url = 'https://saojose.stepzen.net/api/maudlin-oyster/__graphql';
+
+export default function ExercisesScreen() {
+  const { data, isLoading } = useQuery({
+    queryKey: ['exercises'],
+    queryFn: () => {
+      return request({
+        url,
+        document: exerciseQuery,
+        requestHeaders: {
+          authorization:
+            'apikey saojose::stepzen.io+1000::32ca49442c62cd1d0641452cc15ba01130a0c3878fb4b7d39c6433956b7d8d4d',
+        },
+      });
+    },
+  });
+
+  if (isLoading) {
+    return <ActivityIndicator />;
+  }
+
   return (
     <View style={styles.container}>
       <FlashList
-        data={exercises}
+        data={data?.exercises}
         renderItem={renderItem}
         estimatedItemSize={100}
         ItemSeparatorComponent={() => <View style={{ height: 10 }} />}
--- src/types/exercise.ts
+++
@@ -1,9 +0,0 @@
-export type Exercise = {
-  id: number;
-  name: string;
-  type: string;
-  muscle: string;
-  equipment: string;
-  difficulty: string;
-  instructions: string;
-};
